7962009e1902e2b981583cb163da2a0986088fd1
Maruf Hossain <hossainmaruf001@gmail.com>
2023-09-18 13:08:11 +0200

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import BarChart from "./components/BarChart/BarChart";
 import LineChart from "./components/LineChart/LineChart";
 import NavBar from "./components/NavBar/NavBar";
+import Phones from "./components/Phones/Phones";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
 // import DaisyNav from "./components/DaisyNav/DaisyNav";
 
@@ -14,6 +15,7 @@ function App() {
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
       <BarChart></BarChart>
+      <Phones></Phones>
     </>
   );
 }
